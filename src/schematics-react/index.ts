af9139ded55a76c2375c10c5ae5b2f8d55047eac
Jimmy <jiaming0708@gmail.com>
2020-04-06 22:43:59 +0800

    parse name and path

--- src/schematics-react/index.ts
+++ src/schematics-react/index.ts
@@ -1,27 +1,34 @@
-import { Rule, SchematicContext, Tree, apply, url, move, mergeWith, applyTemplates, ActionList } from '@angular-devkit/schematics';
-import { strings } from '@angular-devkit/core';
+import { Rule, SchematicContext, Tree, apply, url, mergeWith, applyTemplates, move } from '@angular-devkit/schematics';
+import { strings, Path, basename, dirname, normalize } from '@angular-devkit/core';
 
 import { OptionSchema } from './schema';
+
+function parseName(path: string, name: string) {
+  const nameWithoutPath = basename(name as Path);
+  const namePath = dirname((path + '/' + name) as Path);
+
+  return {
+    name: nameWithoutPath,
+    path: normalize('/' + namePath),
+  };
+}
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function schematicsReact(_options: OptionSchema): Rule {
   return (_tree: Tree, _context: SchematicContext) => {
     if (!_options.path) {
-      _options.path = './';
+      _options.path = '';
     }
 
-    if (!_options.path.startsWith('/')) {
-    }
-
-    _tree.getDir(_options.name)
+    const parsePath = parseName(_options.path, _options.name);
 
     const templateSource = apply(url('./files'), [
       applyTemplates({
-        ..._options,
         ...strings,
+        ...parsePath,
       }),
-      move(_options.path),
+      move(parsePath.path),
     ]);
 
     return mergeWith(templateSource);
