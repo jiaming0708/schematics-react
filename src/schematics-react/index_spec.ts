121966ef9b4abe3dc721dbd2b4a41ac61f1b78e1
Jimmy <jiaming0708@gmail.com>
2020-04-06 23:02:27 +0800

    write testing

--- src/schematics-react/index_spec.ts
+++ src/schematics-react/index_spec.ts
@@ -2,15 +2,66 @@ import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
+const GENERATE_COMONENT = 'react-new-comp';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
 
 describe('schematics-react', () => {
-  it('works', () => {
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('react-new-comp', {}, Tree.empty());
+  const runner = new SchematicTestRunner('schematics', collectionPath);
 
-    expect(tree.files).toEqual([]);
+  it('create a component', () => {
+    const option = {
+      name: 'button'
+    };
+
+    const tree = runner.runSchematic(GENERATE_COMONENT, option, Tree.empty());
+
+    const files = tree.files;
+
+    expect(files).toEqual(
+      jasmine.arrayContaining([
+        '/Button/Button.jsx',
+        '/Button/Button.scss',
+        '/Button/package.json',
+      ]),
+    );
+  });
+
+  it('create a component with path', () => {
+    const option = {
+      name: 'button',
+      path: 'src',
+    };
+
+    const tree = runner.runSchematic(GENERATE_COMONENT, option, Tree.empty());
+
+    const files = tree.files;
+
+    expect(files).toEqual(
+      jasmine.arrayContaining([
+        '/src/Button/Button.jsx',
+        '/src/Button/Button.scss',
+        '/src/Button/package.json',
+      ]),
+    );
+  });
+
+  it('create a component, the name include path', () => {
+    const option = {
+      name: 'src/button',
+    };
+
+    const tree = runner.runSchematic(GENERATE_COMONENT, option, Tree.empty());
+
+    const files = tree.files;
+
+    expect(files).toEqual(
+      jasmine.arrayContaining([
+        '/src/Button/Button.jsx',
+        '/src/Button/Button.scss',
+        '/src/Button/package.json',
+      ]),
+    );
   });
 });
